a1246d13ed5d7672c68e2ca1951fffa43e51b02e
Christopher Le <christopherle@Christophers-MacBook-Air.local>
2017-09-23 09:59:05 -0700

    second edit

--- cms/.users.js
+++ cms/.users.js
@@ -1,1 +1,11 @@
-{}
+{
+	users: [
+		{
+			username: 'cle',
+			tags: [],
+			salt: '36c48186059123b8167a2032b6590232',
+			hash: '4de71687bdda2270f5f880925c8ff657a6db7e5b3c4990445a2c657c4906b488',
+			user_created_timestamp: 1506149015233
+		}
+	]
+}
--- cms/index.js
+++ cms/index.js
@@ -1,10 +1,10 @@
 {
-	name: 'Julia Smith',
-	specialty: 'Deep learning specialist',
+	name: 'Chris Le',
+	specialty: 'Jr. Full Stack Web Developer',
 	$main_image_type: 'image',
-	main_image: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880097_juliesmith_low.jpg',
+	main_image: '/remote/direct_uploads/1506149121_IMG_0581.jpg',
 	$background_type: 'textarea',
-	background: 'I am a professional software developer with passion for deep learning. After finishing my computer science masters, I worked at Tim Dynamics at a autonomous submarine project. The amazing complexities of developing highly sofisticated diving model was astonishing. Sadly, after the submarine got stuck nearly 2 kilometers under the see surface, our funding was stopped. I became a freelance deep learning specialist and now enjoy diversity of projects ranging from medical research to video game AI.',
+	background: 'I am currently a professional Full Stack Web Developer after taking UCLA\'s  Full Stack Web Development bootcamp. Attended UC Berkeley and graduated with a degree in Political Economy. Started my career in software sales with various tech companies and then transitioned over to customer support for a fin-tech start up.\n\nI decided to learn how to code because I want to stay in tune with technology and the ever changing world. I truly believe that one day, the world will be so immersed in technology that everyone will depend on it in ways that we couldn\'t possibly imagine - I want to be the builder of those types of experiences. Eventually, I want to use my technical and entrepreneurial skills to get into product and build beautiful user experiences for people.',
 	$projects_min: 1,
 	projects: [
 		{
@@ -49,34 +49,28 @@
 	social: [
 		{
 			$icon_type: 'fa',
-			icon: 'github',
-			text: 'Check out my code at github',
-			link: 'https://github.com/Gottwik/Enduro'
-		},
-		{
-			$icon_type: 'fa',
 			icon: 'linkedin',
-			text: 'View my profile at LinkedIn',
-			link: 'https://www.linkedin.com/'
+			text: 'Linkedin',
+			link: 'https://www.linkedin.com/in/le-christopher/'
 		},
 		{
 			$icon_type: 'fa',
-			icon: 'stack-overflow',
-			text: 'I am also active on stackoverflow',
-			link: 'http://stackoverflow.com/cv/martingottweis'
+			icon: 'github',
+			text: 'github',
+			link: 'https://github.com/lechristopher'
 		}
 	],
 	$hobby_background_type: 'image',
-	hobby_background: 'https://s3-eu-west-1.amazonaws.com/enduro.mirror/direct_uploads/1485880478_pexels-photo-92323.jpeg',
-	hobby_title: 'Book enthusiast',
-	hobby_subtitle: 'Books are the old-school internet',
-	cta_tagline: 'Need a deep learning software developer?',
-	cta_text: 'I am actively looking for new challenges',
-	contact_email: 'contact@endurojs.com',
-	footer_text: '2017 @enduro.js mirror theme',
+	hobby_background: '/remote/direct_uploads/1506185298_IMG_0411.JPG',
+	hobby_title: 'Ad-Venture Capitalist',
+	hobby_subtitle: 'Seeking funding in unique experiences.',
+	cta_tagline: 'Looking for a Web Developer?',
+	cta_text: 'Always interested in learning new technologies and the next big thing',
+	contact_email: 'le.christopher92@gmail.com',
+	footer_text: '2017 chrisle',
 	$footer_background_color_type: 'colorpicker',
 	footer_background_color: '#7e0343',
-	page_title: 'Mirror - enduro.js theme',
+	page_title: '',
 	display_edit_stamp: true,
 	$style_expander: true,
 	$style_info: 'won\'t work with the temp action',
@@ -89,6 +83,6 @@
 		text_color: '#2b2928',
 		$accent_color_size: 'inline',
 		$accent_color_type: 'colorpicker',
-		accent_color: '#fafdff',
+		accent_color: '#fafdff'
 	}
 }
